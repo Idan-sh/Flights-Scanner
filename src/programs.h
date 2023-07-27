c855c17d2d0351c174c3100c6e1c81a2813bbdcc
Idan <104992892+Idan-sh@users.noreply.github.com>
2023-07-27 21:44:03 +0300

    Delete src directory

--- src/programs.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROGRAMS_H
-#define PROGRAMS_H
-
-void program1(int argc, char* argv[]);
-void program2(int argc, char *argv[]);
-void program3(int argc, char* argv[]);
-void program4();
-
-#endif // PROGRAMS_H
